6f871ccd968ce698d073bf474e644dc5d844eec7
Loudger <Franticman717@gmail.com>
2024-09-21 15:12:19 +0300

    created enemies and shooting

--- XYZ_GameJam_Project/Source/XYZ_GameJam_Project/XYZ_GameJam_ProjectCharacter.h
+++ XYZ_GameJam_Project/Source/XYZ_GameJam_Project/XYZ_GameJam_ProjectCharacter.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Character.h"
 #include "XYZ_GameJam_ProjectCharacter.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EWeaponFireMode : uint8
+{
+	Single,
+	FullAuto
+};
+
+
 UCLASS(Blueprintable)
 class AXYZ_GameJam_ProjectCharacter : public ACharacter
 {
@@ -28,7 +37,34 @@ public:
 	virtual void MoveRight();
 	virtual void MoveLeft();
 
+	// Fire
+	void StartFire();
+	void StopFire();
+	
+	void MakeShot();
+
+	bool bIsCanShoot;
+
+protected:
+
+	// Скорострельность в минуту
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon | Parameters", meta = (ClampMin = 1.0f, UIMin = 1.0f))
+	float RateOfFire = 300;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Barell attributes | Damage")
+	float DamageAmount = 20.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon | FireMode")
+	EWeaponFireMode FireMode = EWeaponFireMode::FullAuto;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Weapon | Shot")
+	void OnShotEvent();
 private:
+	// Weapon
+	FTimerHandle ShotTimer;
+
+	float GetShotTimerInterval();
+
 	/** Top down camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* TopDownCameraComponent;
